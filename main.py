d626a301c71015a8433c4347e51e9ceb61c07611
jannikpflieger <jannik.pflieger@gmail.com>
2026-02-02 14:13:05 +0100

    cleaned code by removing unnecessary files

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from circuit.pre_processing import convert_to_PCB, convert_to_CliffordT
-from lattice.lattice_layout import create_default_lattice_layout
+
 
 def start_pipline(circuit, lattice_layout = None, is_PCB = False, is_CLiffordT = False):
     if not is_CLiffordT:
         circuit = convert_to_CliffordT(circuit)
     if not is_PCB:
         circuit = convert_to_PCB(circuit)
-    
-    if lattice_layout is None:
-        create_default_lattice_layout(circuit)
-
     return circuit
     
 
